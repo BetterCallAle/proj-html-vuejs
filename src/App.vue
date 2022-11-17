a671fe513783eff4d88e88e13f77504cdffdf980
BetterCallAle <dap22091998@gmail.com>
2022-11-17 11:49:46 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+export default{
+
+}
+</script>
+
+<template>
+  <h1 class="text-center">Hello World!</h1>
+</template>
+
+<style lang="scss">
+// Import the general.scss file
+@use "./styles/general.scss" as *;
+
+</style>
