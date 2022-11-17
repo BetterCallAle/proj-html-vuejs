273e42613736d9bbd4f96100abf1c9e6b5453e83
BetterCallAle <dap22091998@gmail.com>
2022-11-17 17:05:19 +0100

    added navbar in header in a component

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,10 @@
 <script>
+import HeaderNavbar from './HeaderNavbar.vue';
 export default{
     name: "AppHeader",
+    components:{
+        HeaderNavbar
+    },
     data(){
         return{
             links:[
@@ -56,50 +60,8 @@ export default{
                 </a>
             </div>
             <!-- /logo -->
-            
-            <!-- Navbar -->
-            <nav class="header-nav d-flex flex-row-reverse flex-lg-row align-items-center justify-content-start justify-content-lg-end">
-                
-                <!-- Pc menu -->
-                <ul class="d-none d-lg-block">
-                    <li 
-                        v-for="(link, index) in links" 
-                        :key="index"
-                        class="d-inline-block ms_margin"
-                    >
-                        <a :href="link.linkUrl">{{ link.linkTxt }}</a>
-                    </li>
-                </ul>
-                <!-- /Pc menu -->
 
-                <!-- Mobile and Tablet menu -->
-                <button v-if="!isMenuActive" class="ms_btn ms_btn-small ms_btn-black ms_margin d-lg-none" @click="openMenu">
-                    <i class="fa-solid fa-bars"></i>
-                </button>
-
-                <button v-else class="ms_btn ms_btn-small ms_btn-black ms_margin d-lg-none" @click="openMenu">
-                    <i class="fa-solid fa-x"></i>
-                </button>
-                
-                <!-- If the button is clicked -->
-                <div v-if="isMenuActive" class="wall"></div>
-                <div v-if="isMenuActive" class="mobile-menu">
-                    <ul>
-                        <li 
-                            v-for="(link, index) in links" 
-                            :key="index"
-                        >
-                            <a :href="link.linkUrl">{{ link.linkTxt }}</a>
-                        </li>
-                    </ul>
-                </div>
-                <!-- /Mobile and Tablet menu -->
-
-                <a href="#" class="ms_btn ms_btn-small ms_btn-black ms_margin">JOIN US</a>
-
-                <button class="ms_btn-search"><i class="fa-solid fa-magnifying-glass"></i></button>
-            </nav>
-            <!-- /navbar -->
+            <HeaderNavbar :links="links" :menuActive="isMenuActive" @buttonClicked="openMenu"/>
         </div>
     </header>
 </template>
@@ -122,47 +84,12 @@ export default{
             width: 40%;
         }
 
-        .header-nav{
-            width: 60%;
-            color: white;
-
-            .ms_margin{
-                margin-left: 1rem;
-            }
-            .mobile-menu{
-                width: 100%;
-                color: black;
-                text-align: center;
-                background-color: white;
-                border-bottom-left-radius: 30px;
-                border-bottom-right-radius: 30px;
-                box-shadow: 0 300px 1000px 200px rgba(0, 0, 0, .5);
-                position: absolute;
-                top: 100%;
-                left: 50%;
-                transform: translateX(-50%);
-
-                li{
-                    margin-bottom: 1rem;
-                }
-
-            }
-        }
     }
 
 @include for-lg{
     header{
         .header-logo{
             width: 18%;
-        }
-    
-        .header-nav{
-            width:80%;
-
-            .ms_margin{
-                margin-left: 0;
-                margin-right: 3rem;
-            }
         }
     }
 }
---
+++ src/components/HeaderNavbar.vue
@@ -0,0 +1,109 @@
+<script>
+export default {
+  name: "HeaderNavbar",
+  props:{
+    links: Array,
+    menuActive: Boolean
+  }
+};
+</script>
+
+<template>
+  <!-- Navbar -->
+  <nav class="header-nav d-flex flex-row-reverse flex-lg-row align-items-center justify-content-start justify-content-lg-end">
+    <!-- Pc menu -->
+    <ul class="d-none d-lg-block">
+
+      <li
+        v-for="(link, index) in links"
+        :key="index"
+        class="d-inline-block ms_margin"
+      >
+
+        <a :href="link.linkUrl">{{ link.linkTxt }}</a>
+
+      </li>
+    </ul>
+    <!-- /Pc menu -->
+
+    <!-- Mobile and Tablet menu -->
+
+    <!-- If Menu is close -->
+    <button
+      v-if="!menuActive"
+      class="ms_btn ms_btn-small ms_btn-black ms_margin d-lg-none"
+      @click="$emit('buttonClicked')"
+    >
+      <i class="fa-solid fa-bars"></i>
+    </button>
+
+    <!-- If Menu is open -->
+    <button
+      v-else
+      class="ms_btn ms_btn-small ms_btn-black ms_margin d-lg-none"
+      @click="$emit('buttonClicked')"
+    >
+      <i class="fa-solid fa-x"></i>
+    </button>
+
+    <!-- If the button is clicked -->
+    <div v-if="menuActive" class="wall"></div>
+    <div v-if="menuActive" class="mobile-menu">
+      <ul>
+        <li v-for="(link, index) in links" :key="index">
+          <a :href="link.linkUrl">{{ link.linkTxt }}</a>
+        </li>
+      </ul>
+    </div>
+    <!-- /Mobile and Tablet menu -->
+
+    <a href="#" class="ms_btn ms_btn-small ms_btn-black ms_margin">JOIN US</a>
+
+    <button class="ms_btn-search">
+      <i class="fa-solid fa-magnifying-glass"></i>
+    </button>
+    
+  </nav>
+  <!-- /navbar -->
+</template>
+
+<style lang="scss" scoped>
+    @use "../styles/partials/variables" as *;
+    @use "../styles/partials/mixin" as *;
+    .header-nav {
+    width: 60%;
+    color: white;
+
+    .ms_margin {
+        margin-left: 1rem;
+    }
+    .mobile-menu {
+        width: 100%;
+        color: black;
+        text-align: center;
+        background-color: white;
+        border-bottom-left-radius: 30px;
+        border-bottom-right-radius: 30px;
+        box-shadow: 0 300px 1000px 200px rgba(0, 0, 0, 0.5);
+        position: absolute;
+        top: 100%;
+        left: 50%;
+        transform: translateX(-50%);
+
+        li {
+        margin-bottom: 1rem;
+        }
+    }
+    }
+
+    @include for-lg {
+    .header-nav {
+        width: 80%;
+
+        .ms_margin {
+        margin-left: 0;
+        margin-right: 3rem;
+        }
+    }
+    }
+</style>
